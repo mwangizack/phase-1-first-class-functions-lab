4056276a1b0bd01586fb57e9fb4326452821abb2
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-03-30 15:21:34 +0300

    Write higher-order functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = arrayofDrivers => arrayofDrivers.slice(0,2);
+
+const returnLastTwoDrivers = arrayofDrivers => arrayofDrivers.slice(arrayofDrivers.length - 2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+    return (fare) => fare * num;
+}
+
+const fareDoubler = fare => createFareMultiplier(2)(fare);
+
+const fareTripler = fare => createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(arrayOfDrivers, returnDrivers){
+    return returnDrivers(arrayOfDrivers);
+}
+
+ console.log(selectDifferentDrivers([1,2,3,4,5], returnLastTwoDrivers))
